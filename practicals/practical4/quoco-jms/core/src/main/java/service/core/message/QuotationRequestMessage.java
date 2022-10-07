8a2fa4257172e944839d76d47e5b541f9dc006f2
eoinsheehan <eoin.sheehan@ucdconnect.ie>
2022-10-07 09:38:53 +0100

    Fix package naming convention

---
+++ practicals/practical4/quoco-jms/core/src/main/java/service/core/message/QuotationRequestMessage.java
@@ -0,0 +1,14 @@
+package service.core.message;
+
+import service.core.ClientInfo;
+
+public class QuotationRequestMessage implements java.io.Serializable {
+ public long id;
+ public ClientInfo info;
+
+ public QuotationRequestMessage(long id, ClientInfo info) {
+ this.id = id;
+ this.info = info;
+ }
+} 
+
---
+++ practicals/practical4/quoco-jms/core/src/main/java/service/core/message/QuotationResponseMessage.java
@@ -0,0 +1,14 @@
+package service.core.message;
+
+import service.core.ClientInfo;
+import service.core.Quotation;
+
+public class QuotationResponseMessage implements java.io.Serializable {
+ public long id;
+ public Quotation quotation;
+
+ public QuotationResponseMessage(long id, Quotation quotation) {
+ this.id = id;
+ this.quotation = quotation;
+ }
+} 
